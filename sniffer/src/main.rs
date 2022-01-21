0f477777e8495da8582f8f2170668611b0304e6c
JP Dube <jpdube@numa-informatique.com>
2022-01-20 20:43:48 -0500

    Added pql lib support

--- sniffer/src/main.rs
+++ sniffer/src/main.rs
@@ -1,33 +1,29 @@
 use std::env;
 
 // mod db;
-mod packetref;
-mod sniffer;
 mod config;
 mod db_config;
 mod dbmongo;
+mod packetref;
+mod sniffer;
 use config::Config;
 use dbmongo::MDatabase;
 use pql;
 
-
-
 #[warn(dead_code)]
 fn main() {
     println!("Packet capture");
-    
     let config: Config = config::read(&"./config/config.yaml");
     println!("Db path: {}", config.db_path);
-    
     // let database: MDatabase = MDatabase::new();
     // database.init();
 
-    pql::main();
+    // pql::main();
 
     let args: Vec<String> = env::args().collect();
     if args.len() == 2 {
         println!("Target device is: {}", args[1]);
-        sniffer::capture(&args[1]);
+        sniffer::capture(&args[1]).unwrap();
     }
     println!("{:?}", args);
     // match database() {
@@ -35,4 +31,3 @@ fn main() {
     // Ok(()) => {}
     // }
 }
-

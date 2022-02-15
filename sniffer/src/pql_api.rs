e85d75a2ff821d19a07d300c33f8712b7306ad3f
JP Dube <jpdube@numa-informatique.com>
2022-02-15 16:07:47 -0500

    Code cleanup

--- sniffer/src/pql_api.rs
+++ sniffer/src/pql_api.rs
@@ -1,11 +1,12 @@
-use actix_web::{web, App, HttpServer};
 use actix_web::Responder;
+use actix_web::{web, App, HttpServer};
+use serde::{Deserialize, Serialize};
 use std::thread;
-use serde::{Serialize, Deserialize}; 
 
 struct ApiControl {
     stop: bool,
 }
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PqlQuery {
     pub query: String,
@@ -14,15 +15,14 @@ pub struct PqlQuery {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct QueryResp {
     pub query: String,
-    pub response: String
+    pub response: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct User {
     pub username: String,
-    pub password: String
+    pub password: String,
 }
-
 
 #[actix_rt::main]
 async fn main_api() -> std::io::Result<()> {
@@ -35,32 +35,27 @@ async fn main_api() -> std::io::Result<()> {
             // .route("/users/{id}", web::get().to(get_user_by_id))
             .route("/query", web::post().to(exec_query))
             .route("/login", web::post().to(login))
-            // .route("/users/{id}", web::delete().to(delete_user))
+        // .route("/users/{id}", web::delete().to(delete_user))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
 
-
 pub async fn login(item: web::Json<User>) -> impl Responder {
-
-
     format!("Username {}, Password {}", item.username, item.password)
 }
 
 pub async fn exec_query(item: web::Json<PqlQuery>) -> impl Responder {
     let resp = QueryResp {
         query: item.query.clone(),
-        response: "This is the response!!!".to_string()
+        response: "This is the response!!!".to_string(),
     };
 
     format!("Query {:?}", resp)
 }
 
-
-pub fn start () {
-
+pub fn start() {
     thread::spawn(|| {
         main_api();
     });
